b83ac403220ec762d9336afbc9f8842e83411f6f
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-09 21:42:53 +0100

    added a no wrap to the hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="mt-20">
         <h1 className="font-bold text-center text-3xl pb-3  md:text-6xl md:ml-28">
           More than just <br className="hidden md:block"/>
-          <span className="underline">shorter links</span>
+          <span className="underline text-nowrap">shorter links</span>
         </h1>
         <p className="ml-2 md:ml-28">
           Build your brand's recognition and get detailed insights <br />
