ebd2a0678bbeaa45a627ace225d54e088507313e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-19 14:09:51 +0100

    made a re-adjustment to the boxes

--- src/components/Advanced.jsx
+++ src/components/Advanced.jsx
@@ -8,8 +8,8 @@ const Advanced = () => {
         Track how your link are performing across the web with <br className="hidden md:block"/>
         our advanced statistics dashboard
       </p>
-      <div className="md:grid grid-cols-3 mb-20 md:ml-20 md:mr-20">
-        <div className="shadow-2xl mb-14 p-10 m-5 ">
+      <div className="lg:grid grid-cols-3 mb-20 md:ml-20 md:mr-20">
+        <div className="shadow-2xl mb-14 p-10 m-5">
           <h3 className="font-bold text-2xl pb-3">Brand Recognition</h3>
           <p>
             Boost your brand recognition with each click. Generic links don't
